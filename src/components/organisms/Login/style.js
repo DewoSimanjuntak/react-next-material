40d50a9532e6b1c012e93b94ec2fbefda6e5c9ff
Pratama Indra Raswoko <pratamaindra.r@photoninfotech.net>
2022-07-18 15:11:46 +0700

    update login component

---
+++ src/components/organisms/Login/style.js
@@ -0,0 +1,34 @@
+
+export const styles = {
+    container: {
+        width: 300,
+        padding: "20px 10px 15px",
+        backgroundColor: "white",
+        alignSelf: "center",
+        margin: "auto",
+        borderRadius: "5px"
+    },
+    title: {
+       fontSize: 25 
+
+    },
+    link: {
+
+    },
+    createAccountText: {
+
+    },
+    containedButton: {
+        backgroundColor: "#E5EDF8P",
+        borderRadius: 46,
+        color: "white",
+        "&:hover": {
+            backgroundColor: "#E5EDF8P",
+        },
+    },
+    outlinedButton: {
+        borderRadius: 46,
+    },
+
+
+}
