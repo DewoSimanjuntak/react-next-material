cb50edba39a78a37e1fe4cbba64bb982bd322db4
Fajar Apriansyah <fajar.apriansyah@photon.com>
2022-07-19 15:43:35 +0700

    fix: authLayout component-container when the height is more than 100vh

--- src/components/templates/authLayout.jsx
+++ src/components/templates/authLayout.jsx
@@ -10,15 +10,15 @@ export default function Layout({ children }) {
       </Head>
       <div className={styles.authLayout}>
         <div className={styles.authContainer}>
-          <section className={styles.authComponentContainer}>
+          <section className={[styles.authComponentContainer, 'hide-scrollbar'].join(' ')}>
             {children}
           </section>
           
           <section className={styles.authImageContainer}>
-            <img
+            {/* <img
               src="https://c4.wallpaperflare.com/wallpaper/930/115/679/panda-4k-high-quality-hd-wallpaper-preview.jpg"
               className={styles.imageBanner}
-            />
+            /> */}
           </section>
         </div>
       </div>
--- src/pages/forgot-password.js
+++ src/pages/forgot-password.js
@@ -1,10 +1,11 @@
+import AuthLayout from '../components/templates/authLayout';
 import ForgotPasswordComponent from "../components/organisms/ForgotPassword/forgotPassword";
 import SetPasswordComponent from "../components/organisms/SetPassword/setPassword";
-import styles from "../../styles/Login.module.css";
+import styles from "../../styles/ForgotPassword.module.css";
 
 export default function ForgotPasswordPage() {
   return (
-    <div className={styles.forgotPasswordPage}>
+    <div className={[styles.forgotPasswordPage, 'hide-scrollbar'].join(' ')}>
       <section className={styles.forgotPasswordComponentContainer}>
         <ForgotPasswordComponent />
       </section>
@@ -13,4 +14,12 @@ export default function ForgotPasswordPage() {
       </section>
     </div>
   );
+}
+
+ForgotPasswordPage.getLayout = function getLayout(page) {
+  return (
+    <AuthLayout>
+      {page}
+    </AuthLayout>
+  )
 }
