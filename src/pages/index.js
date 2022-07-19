087f61be32d303804a3152ddcd009e104166f432
Fajar Apriansyah <fajar.apriansyah@photon.com>
2022-07-19 21:02:52 +0700

    chore: add hooks example for data fetching with SWR

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import { Skeleton, Stack } from '@mui/material';
+import { useGetUserData } from "../../hooks/useUser";
 import DefaultLayout from '../components/templates/default';
 
 export default function Home() {
+  const { data, error } = useGetUserData()
+
+  if (error) console.log({error})
+  const userData = data || {}
+
   return (
     <Stack spacing={2} padding={4}>
       <div>This is Homepage</div>
+      <div>User Data: {userData.firstName} {userData.lastName}</div>
       <Skeleton variant="rectangular" width={'50%'} height={118} />
       <Skeleton variant="rectangular" width={210} height={118} />
     </Stack>
