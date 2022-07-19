c39d320180c8512ee93e8ee232e948297a9729f1
Putra Aditya Wicaksono <putra.aditya@photoninfotech.net>
2022-07-19 20:48:56 +0700

    Initial commit to environment variable

--- next.config.js
+++ next.config.js
@@ -7,6 +7,19 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  env:{
+    // Set PORT to environtment variable (e.g: PORT=3005 npm run dev)
+    PORT: process.env.PORT
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret',
+    secondSecret: process.env.SECOND_SECRET, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/static',
+  },
 };
 
 module.exports = nextConfig;
