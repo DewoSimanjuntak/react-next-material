fd4ecb9099f8fd013a552185d586c547209858f5
Pratama Indra Raswoko <pratamaindra.r@photoninfotech.net>
2022-07-21 18:33:38 +0700

    add base header and update the responsive page

--- src/components/templates/authLayout.jsx
+++ src/components/templates/authLayout.jsx
@@ -1,25 +1,28 @@
 import Head from "next/head";
 import styles from "./authLayout.module.scss";
 import { colors } from "../../styles/theme";
+import BaseHeader from "../organisms/BaseHeader/baseHeader"; "../../components/organisms/BaseHeader/baseHeader"
+import Container from '@mui/material/Container';
 
-export default function Layout({ children }) {
+export default function Layout({ children, showImage=false }) {
   return (
     <>
       <Head>
         <title>Layouts Example</title>
       </Head>
       <div className={styles.authLayout}>
+        <BaseHeader></BaseHeader>
         <div className={styles.authContainer}>
-          <section className={styles.authComponentContainer}>
+          <Container className={styles.authComponentContainer} sx={{paddingTop: { xs: showImage ? '35px' : '75px'}, padding: 0}}>
             {children}
-          </section>
+          </Container>
 
-          <section className={styles.authImageContainer}>
+          <Container className={styles.authImageContainer} sx={{display: { xs: showImage ? 'flex' : 'none'}, padding: 0}}>
             <img
               src="https://c4.wallpaperflare.com/wallpaper/930/115/679/panda-4k-high-quality-hd-wallpaper-preview.jpg"
               className={styles.imageBanner}
             />
-          </section>
+          </Container>
         </div>
       </div>
     </>
