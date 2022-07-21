df79a67ca6158c4753d68f22437bc1cefbed557e
Ilman Teguh Prasetya <ilman.teguh@photon.com>
2022-07-21 16:33:44 +0700

    update primary to secondary button

--- src/components/organisms/Login/login.jsx
+++ src/components/organisms/Login/login.jsx
@@ -75,7 +75,7 @@ export default function Login({
         </StyledButton>
         <StyledButton
           theme="patient"
-          type = "primary"
+          type = "secondary"
           size="large"
           gradient={false}
           onClick={OnGuestClicked}
@@ -88,8 +88,8 @@ export default function Login({
         </Grid>
 
         <StyledButton
-          theme="patient"
-          type = "primary"
+        theme="patient"
+          type = "secondary"
           size="large"
           gradient={false}
           onClick={function () {
