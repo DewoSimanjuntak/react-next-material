73ed755df8de2764a131f649c0f049d27f2abc58
Fajar Apriansyah <fajar.apriansyah@photon.com>
2022-07-25 14:41:12 +0700

    fix: re-enable import BaseHeader component function in authLayout

--- src/components/templates/authLayout.jsx
+++ src/components/templates/authLayout.jsx
@@ -4,6 +4,15 @@ import { colors } from "../../styles/theme";
 import Container from '@mui/material/Container';
 import { patientTypography, providerTypography } from "../../styles/theme"
 import { ThemeProvider} from "@mui/material/styles";
+import dynamic from "next/dynamic";
+
+//Prevent html being match between server and client
+const BaseHeader = dynamic(
+  () => import("../organisms/BaseHeader/baseHeader"),
+  {
+    ssr: false,
+  }
+);
 
 export default function Layout({ children, showMobileImage=false, theme = "patient" }) {
   const isPatient = theme === "patient"
